80f885d2892fcc3471f90f9f9124b61cec5eb11f
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-06-30 00:29:11 +0330

    chore: renamed Folder Domian to Domain and Manever.cpp to ManeverUseCase.cpp

---
+++ build/CMakeFiles/unmatched.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unmatched.dir/main.cpp.o"
+  "CMakeFiles/unmatched.dir/main.cpp.o.d"
+  "CMakeFiles/unmatched.dir/src/Application/UseCases/Manever.cpp.o"
+  "CMakeFiles/unmatched.dir/src/Application/UseCases/Manever.cpp.o.d"
+  "CMakeFiles/unmatched.dir/src/Domain/Entities/Graph.cpp.o"
+  "CMakeFiles/unmatched.dir/src/Domain/Entities/Graph.cpp.o.d"
+  "CMakeFiles/unmatched.dir/src/Domain/Entities/Hero.cpp.o"
+  "CMakeFiles/unmatched.dir/src/Domain/Entities/Hero.cpp.o.d"
+  "unmatched"
+  "unmatched.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unmatched.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/unmatched.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/unmatched.dir/main.cpp.o: \
+ /home/lionborn/programs/projects/PROJECT/Cobble\ &\ fog/Cobble-Fog/main.cpp \
+ /usr/include/stdc-predef.h
