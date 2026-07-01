238fe3fde0725678fb652404b050f1c52a089941
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-01 23:13:37 +0330

    add struct Combat Participant

---
+++ include/Domain/Combat/CombatParticipant.h
@@ -0,0 +1,13 @@
+#ifndef COMBAT_PARTICIPANT
+#define COMBAT_PARTICIPANT
+#include "Domain/Entities/Fighter.h"
+#include "Domain/Entities/Hero.h"
+#include "Domain/Entities/Card.h"
+struct CombatParticipant
+{
+    Hero & hero;
+    Fighter & fighter;
+    Card & card;
+};
+
+#endif /* COMBAT_PARTICIPANT */
