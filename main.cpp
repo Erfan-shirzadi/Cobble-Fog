0ad27a9139ee03caa7bf15ab2958609ed1f01de8
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-01 21:15:08 +0330

    main for test

--- main.cpp
+++ main.cpp
@@ -3,23 +3,22 @@
 #include "Application/UseCases/ManeverUseCase.h"
 #include <iostream>
 #include <vector>
-
+#include "Domain/Entities/Board.h"
 using namespace std;
 
 int main(){
 
-    ManeverUseCase m;
-    Graph map;
+    Board board;
     Holmes h;
     Dracula d;
     h.SetNode(1);
-    d.SetNode(1);
+    d.SetNode(4);
     cout<<h.GetName()<<" hp:"<<h.GetHP()<<" "<<h.GetNode()<< " "<<h.GetMove()<<endl;
     cout<<d.GetName()<<" hp:"<<d.GetHP()<<" "<<d.GetNode()<< " "<<d.GetMove()<<endl;
 
     std::vector<int> nodes;
 
-    nodes=m.reachableNodesCurrentHero(map,h,16);
+    nodes=board.reachableNodes(h,d,h.GetMove(),h.GetNode());
     for(int node:nodes)
         cout << node <<" ";
 
