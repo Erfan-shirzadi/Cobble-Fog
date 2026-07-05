70345ac0937a3c5702de6bbc625b2e14872cde8a
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-05 12:18:19 +0330

    feat: interface Scheme Card

---
+++ include/Domain/Entities/SchemeCard.h
@@ -0,0 +1,12 @@
+#ifndef SCHEME_CARD
+#define SCHEME_CARD
+#include "Domain/Entities/Card.h"
+#include "Domain/Entities/Hero.h"
+#include "Domain/Entities/Board.h"
+class SchemeCard : public Card{
+
+    public:
+    virtual void Play(Hero * ,Hero * , Board *)=0;
+};
+
+#endif /* SCHEME_CARD */
