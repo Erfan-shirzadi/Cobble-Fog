dff5267998c285d084f125ea12060ca9e43f88ee
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-06 11:56:08 +0330

    feat: add Class deck

---
+++ include/Domain/Entities/Deck.h
@@ -0,0 +1,21 @@
+#ifndef DECK_H
+#define DECK_H
+#include "Domain/Entities/Card.h"
+#include <vector>
+#include <memory>
+
+class Deck{
+
+    std::vector<std::unique_ptr<Card>> deck;
+
+    public:
+
+    void Shuffle();
+    void Add(std::unique_ptr<Card>);
+    std::unique_ptr<Card> Draw();
+    bool Empty()const;
+    int Size()const;
+
+};
+
+#endif /* DECK_H */
---
+++ src/Domain/Entities/Deck.cpp
@@ -0,0 +1,25 @@
+#include "Domain/Entities/Deck.h"
+#include <random>
+#include <algorithm>
+
+
+void Deck::Shuffle(){
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    std::shuffle(deck.begin(),deck.end(),generator);
+}
+void Deck::Add(std::unique_ptr<Card> card){
+    this->deck.push_back(std::move(card));
+}
+std::unique_ptr<Card> Deck::Draw(){
+    auto card = std::move(deck.back());
+    deck.pop_back();
+    return card;
+}
+bool Deck::Empty()const{
+    return this->deck.size()==0;
+}
+int Deck::Size()const{
+    return this->deck.size();
+}
