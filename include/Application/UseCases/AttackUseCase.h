06d54f9046f4042ca32ebd42ae5e5e7970280b9f
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-17 13:10:53 +0330

    feat: add Interface Attack Use Case

--- include/Application/UseCases/AttackUseCase.h
+++ include/Application/UseCases/AttackUseCase.h
@@ -3,10 +3,28 @@
 #include "Domain/Game/GameState.h"
 #include "Domain/Combat/CombatContext.h"
 #include "Application/UseCases/IUseCase.h"
+#include "Application/UseCases/CombatUseCase.h"
 
+enum class AttackStep{
+    SETUP,
+    COMBAT,
+    FINISHED
+};
+
+enum class SetUpStep{
+    CHOOSE_ATTACKER,
+    CHOOSE_ATTACKER_CARD,
+    CHOOSE_DEFFENDER,
+    CHOOSE_DEFFENDER_CARD
+};
 class AttackUseCase : public IUseCase {
     
     CombatContext context;
+    CombatUseCase combat;
+
+    SetUpStep setupstep;
+    AttackStep attackstep;
+
     
     public:
     bool execute(GameState & );
@@ -21,6 +39,9 @@ class AttackUseCase : public IUseCase {
 
     ContinueResult Continue(ActionContext&)override;
     void Start(ActionContext&)override;
+    ContinueResult SetUp(ActionContext&);
+    ContinueResult Finished(ActionContext&);
+    ContinueResult Combat(ActionContext&);
 
 
 };
