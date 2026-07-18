2d4685dd9f0b5296c6ba142300f674d66472f9a9
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-19 01:50:50 +0330

    feat: add Eliminate The Impossible Effect

---
+++ include/Application/CardEffect/HolmesCardsEffect/EliminateTheImpossible.h
@@ -0,0 +1,12 @@
+#ifndef ELIMINATE_THE_IMPOSSIBLE_EFFECT
+#define ELIMINATE_THE_IMPOSSIBLE_EFFECT
+#include "Application/CardEffect/CardEffect.h"
+
+class EliminateTheImpossibleEffect :public CardEffect{
+
+    public: 
+    ContinueResult Continue(EffectContext&);
+    ContinueResult BuildCardMenu(EffectContext&);
+};
+
+#endif /* ELIMINATE_THE_IMPOSSIBLE_EFFECT */
---
+++ src/Application/CardEffect/HolmesCardsEffect/EliminateTheImpossibleEffect.cpp
@@ -0,0 +1,26 @@
+#include "Application/CardEffect/HolmesCardsEffect/EliminateTheImpossible.h"
+
+   
+ContinueResult EliminateTheImpossibleEffect::Continue(EffectContext& context){
+    if(context.context.Selected==-1) return BuildCardMenu(context);
+
+    context.context.Gamestate->opponentPlayre->GetHero()->GetCard(context.context.Selected);
+
+    ContinueResult res;
+     res.status= ContinueStatus::FINISHED;
+
+     return res;
+}
+
+
+ContinueResult EliminateTheImpossibleEffect::BuildCardMenu(EffectContext& context){
+    ContinueResult result;
+    for(auto card : context.context.Gamestate->opponentPlayre->GetHero()->GetHand()){
+        result.menu_request.options.push_back(card->GetName());
+    }
+
+    result.menu_request.title="Enemy Cards";
+    result.status=ContinueStatus::NEEDMENU;
+
+    return result;
+}
