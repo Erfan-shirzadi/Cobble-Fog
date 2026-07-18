8958624afd67906af11a91abcc487794998d2d60
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-07-19 02:10:19 +0330

    feat: add Fixed A point IN a Changing Age Effect:

---
+++ include/Application/CardEffect/HolmesCardsEffect/FixedAPointInChangingAgeEffect.h
@@ -0,0 +1,11 @@
+#ifndef FIXED_A_POINT_IN_CHANGING_AGE_EFFECT
+#define FIXED_A_POINT_IN_CHANGING_AGE_EFFECT
+#include "Application/CardEffect/CardEffect.h"
+
+class FixedAPointInChangingAgeEffect : public CardEffect{
+
+    public:
+    ContinueResult Continue(EffectContext context);
+};
+
+#endif /* FIXED_A_POINT_IN_CHANGING_AGE_EFFECT */
---
+++ src/Application/CardEffect/HolmesCardsEffect/FixedAPointInChangingAgeEffect.cpp
@@ -0,0 +1,16 @@
+#include "Application/CardEffect/HolmesCardsEffect/FixedAPointInChangingAgeEffect.h"
+
+ContinueResult FixedAPointInChangingAgeEffect::Continue(EffectContext context){
+    Hero * holmes=context.context.Gamestate->currnetPlayer->GetHero();
+    std::vector<Fighter*> watson=holmes->GetSideKicks();
+    Board board=context.context.Gamestate->board;
+    if(board.AreAdjacent(holmes->GetNode(),watson[0]->GetNode())){
+        holmes->Heal(1);
+        watson[0]->Heal(1);
+    }
+
+    ContinueResult res;
+     res.status=ContinueStatus::FINISHED;
+     return res;
+
+}
