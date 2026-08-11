893f0c834fbdcd72313ac9b8c892267518999999
Erfan-Shirzadi <erfanshirzadiiiii@gmail.com>
2026-08-11 13:28:30 +0330

    feat: add Cards To deck

--- src/Domain/Entities/Heroes/InvisibleMan.cpp
+++ src/Domain/Entities/Heroes/InvisibleMan.cpp
@@ -1,13 +1,65 @@
 #include "Domain/Entities/Heroes/InvisibleMan.h"
+#include "Domain/Entities/Cards/InvisibleMan/CodedNotes.h"
+#include "Domain/Entities/Cards/InvisibleMan/DreamingOfRevenge.h"
+#include "Domain/Entities/Cards/InvisibleMan/EmergeFromMist.h"
+#include "Domain/Entities/Cards/InvisibleMan/ImpossibleToSee.h"
+#include "Domain/Entities/Cards/InvisibleMan/IntoThinAir.h"
+#include "Domain/Entities/Cards/InvisibleMan/Lurking.h"
+#include "Domain/Entities/Cards/InvisibleMan/ReignOfTerror.h"
+#include "Domain/Entities/Cards/InvisibleMan/RollingFog.h"
+#include "Domain/Entities/Cards/InvisibleMan/SlipAway.h"
+#include "Domain/Entities/Cards/InvisibleMan/StepLightly.h"
 
 InvisibleMan::InvisibleMan():Hero("Invisible Man",15,2,FighterType::INVISIBLEMAN){
-    SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
-    SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
-    SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
+    // SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
+    // SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
+    // SideKicks.push_back(std::make_unique<Fighter>("Fog",1,0,FighterType::FOG));
     
+    AddCardToDeck(std::make_unique<CodedNotes>());
+    AddCardToDeck(std::make_unique<CodedNotes>());
+
+    AddCardToDeck(std::make_unique<DreaminOfRevenge>());
+    AddCardToDeck(std::make_unique<DreaminOfRevenge>());
+
+    AddCardToDeck(std::make_unique<EmergeFromMist>());
+    AddCardToDeck(std::make_unique<EmergeFromMist>());
+
+
+    AddCardToDeck(std::make_unique<ImpossibleToSee>());
+    AddCardToDeck(std::make_unique<ImpossibleToSee>());
+
+    AddCardToDeck(std::make_unique<IntoThinAir>());
+    AddCardToDeck(std::make_unique<IntoThinAir>());
+
+    AddCardToDeck(std::make_unique<Lurking>());
+    AddCardToDeck(std::make_unique<Lurking>());
+
+    AddCardToDeck(std::make_unique<ReignOfTerror>());
+    AddCardToDeck(std::make_unique<ReignOfTerror>());
+
+    AddCardToDeck(std::make_unique<RollingFog>());
+    AddCardToDeck(std::make_unique<RollingFog>());
+
+    AddCardToDeck(std::make_unique<SlipAway>());
+    AddCardToDeck(std::make_unique<SlipAway>());
+    AddCardToDeck(std::make_unique<SlipAway>());
+
+    
+    AddCardToDeck(std::make_unique<StepLightly>());
+    AddCardToDeck(std::make_unique<StepLightly>());
+
+
+
+
+
 
 }
 
-std::vector<Fighter *> InvisibleMan::GetSideKicks(){
 
+std::vector<Fighter *> InvisibleMan::GetSideKicks() {
+    return {nullptr};
 }
+Fighter * InvisibleMan::GetDeadSideKick()const {
+    return nullptr;
+}
+
